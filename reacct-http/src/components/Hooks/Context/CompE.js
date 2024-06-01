364558cf4063d312dda4004b55e4a5e8822e148c
Aditi-R-Vadi <vadiaditi@gmail.com>
2024-06-01 15:25:15 +0530

    useContext easy

--- reacct-http/src/components/Hooks/Context/CompE.js
+++ reacct-http/src/components/Hooks/Context/CompE.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import CompF from './CompF'
+import { UserContext,ChannelContext } from '../../../App'
 
 function CompE() {
+    const user=useContext(UserContext)
+    const channal=useContext(ChannelContext)
   return (
     <div>
-      <CompF/>
+      {user}-{channal}
     </div>
   )
 }
