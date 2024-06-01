0218a1847f56999725767eff3ac0ec3965e64326
Aditi-R-Vadi <vadiaditi@gmail.com>
2024-06-01 12:03:40 +0530

    useState with Array

--- reacct-http/src/App.js
+++ reacct-http/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
-import HokkCounterThree from './components/Hooks/HokkCounterThree';
+import HookswithArray from './components/Hooks/HookswithArray';
+//import HokkCounterThree from './components/Hooks/HokkCounterThree';
 //import HookCounter from './components/Hooks/HookCounter';
 //import HookCounterTwo from './components/Hooks/HookCounterTwo';
 //import PostForm from './components/PostForm';
@@ -12,8 +13,9 @@ function App() {
       {/*<PostList/>
       <PostForm/>
       <HookCounter/>
-      <HookCounterTwo/>*/}
-      <HokkCounterThree/>
+      <HookCounterTwo/>
+      <HokkCounterThree/>*/}
+      <HookswithArray/>
     </div>
   );
 }
