9eef825b6b4658098def834eea46d2f412197cad
Aditi-R-Vadi <vadiaditi@gmail.com>
2024-06-03 16:00:37 +0530

    Parenet and Child Render

--- reacct-http/src/App.js
+++ reacct-http/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import './App.css';
-import { ObjectUseState } from './components/Rect_Render/Immutable_State/ObjectUseState';
-import { ArrayUseState } from './components/Rect_Render/Immutable_State/ArrayUseState';
+import { Parent } from './components/Rect_Render/Parent_Child/Parent';
+
 
 
 function App() {
   return (
     <div className='App'>
-     <ObjectUseState/>
-     <ArrayUseState/>
+    <Parent/>
     </div>
   );
 }
