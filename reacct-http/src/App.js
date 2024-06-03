e5678efc90bef9214d2e259addf3d13cc40e829f
Aditi-R-Vadi <vadiaditi@gmail.com>
2024-06-03 10:41:00 +0530

    React Render by useState

--- reacct-http/src/App.js
+++ reacct-http/src/App.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import './App.css';
-import DocTitleOne from './components/Custom_Hooks/DocTitleOne';
-import DocTitleTwo from './components/Custom_Hooks/DocTitelTwo';
-import CounterOne from './components/Custom_Hooks/CounterOne';
-import CounterTwo from './components/Custom_Hooks/CounterTwo';
-import UserForm from './components/Custom_Hooks/UserForm';
+import UseStateHook from './components/Rect_Render/UseState/UseStateHook';
+
 
 function App() {
   return (
     <div className='App'>
-      <DocTitleOne/>
-      <DocTitleTwo/>
-      <CounterOne/>
-      <CounterTwo/>
-      <UserForm/>
+     <UseStateHook/>
     </div>
   );
 }
